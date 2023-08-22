dc052cef5e1b3fcba7be93d796eb63703db337e9
iSaadiqbal <139994436+iSaadiqbal@users.noreply.github.com>
2023-08-23 02:23:29 +0500

    Update python-scripts.py

--- python-scripts.py
+++ python-scripts.py
@@ -24,15 +24,21 @@ def send_email(subject, body, recipient_emails):
         print("An error occurred:", e)
 
 # Get the result of the DNS record update from the previous step
-dns_details = os.environ.get("DNS_DETAILS", "N/A")
+dns_name = os.environ.get("DNS_NAME", "N/A")
+dns_type = os.environ.get("DNS_TYPE", "N/A")
+dns_ip = os.environ.get("DNS_IP", "N/A")
+
 dns_result = f"""
 DNS record updated by workflow. Trigger count: {trigger_count}
 
-DNS Details: {dns_details}
+DNS Details:
+Name: {dns_name}
+Type: {dns_type}
+IP Address: {dns_ip}
 """
 
 # List of recipient email addresses
-recipient_emails = ["saad89.linux@gmail.com"]
+recipient_emails = ["saad89.linux@gmail.com", "usamashahid3565@gmail.com", "tayyubtahir87@gmail.com"]
 
 # Call the send_email function
 send_email("DNS Record Update Result", dns_result, recipient_emails)
