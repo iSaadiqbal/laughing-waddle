17d073d43b76bbb101694937c0a0abafa2f0fadd
iSaadiqbal <139994436+iSaadiqbal@users.noreply.github.com>
2023-08-23 01:42:17 +0500

    new scripts with additional details

--- python-scripts.py
+++ python-scripts.py
@@ -21,25 +21,17 @@ def send_email(subject, body, recipient_emails):
 
 # Get the result of the DNS record update from the previous step
 dns_result = """
-DNS record updated by workflow. Trigger count: 5
+DNS record updated by workflow. Trigger count: $GITHUB_RUN_NUMBER
+DNS details:
+Record Name: saad.karazo.com
+Record Type: CNAME
+Record Content: YOUR_IP_ADDRESS
+TTL: 3600
+Comment: Domain verification record
 """
 
 # List of recipient email addresses
-recipient_emails = ["saad89.linux@gmail.com", "usamashahid3565@gmail.com", "tayyubtahir87@gmail.com"]
-
-# Extract DNS details from your workflow
-zone_id = "38b42bfdb42dbe301b6b1a27b86ac939"
-record_name = "saad.karazo.com"
-record_type = "CNAME"
-record_content = "192.168.18.250"
-ttl = 3600
-
-# Construct the email subject and body
-trigger_count = 5  # Replace with the actual trigger count from the workflow
-email_subject = f"DNS Record Update Result - Trigger count: {trigger_count}"
-email_body = f"{dns_result}\n\n"
-email_body += f"Zone ID: {zone_id}\nRecord Name: {record_name}\nRecord Type: {record_type}\n"
-email_body += f"Record Content: {record_content}\nTTL: {ttl}"
+recipient_emails = ["saad89.linux@gmail.com"]
 
 # Call the send_email function
-send_email(email_subject, email_body, recipient_emails)
+send_email("DNS Record Update Result (Trigger #$GITHUB_RUN_NUMBER)", dns_result, recipient_emails)
