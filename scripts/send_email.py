b9361c1333032bca8eff871012f0efd661a69291
Tayyab <127115509+tayyabmoshin@users.noreply.github.com>
2023-08-22 16:45:33 +0500

    My email.info

--- scripts/send_email.py
+++ scripts/send_email.py
@@ -3,8 +3,8 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 # Email configuration
-sender_email = "saadiqbalbutt89@gmail.com"
-receiver_email = "saad89.linux@gmail.com"
+sender_email = "tayyubtahir87@gmail.com"
+receiver_email = "tayyubtahir87@gmail.com"
 subject = "Static Website HTML"
 
 # Read HTML content from index.html
@@ -13,10 +13,7 @@ with open("index.html", "r") as html_file:
 
 # Additional text
 additional_text = """
-Hello bahi, I am using python file to add HTMl code in the body of the email. I tried with many workflow formatts from the
-github none of them worked with the HTML i was not able to add it in the email body.  so i found a way to do it through the python code.
-then i learned how to make my .py file work with the yaml file. 
-and now i have the HTML code of the site below as text.
+hello farman bhai. i have completed my task.
 
 ---
 
@@ -25,7 +22,7 @@ and now i have the HTML code of the site below as text.
 ---
 
 Thank you,
-Saad
+Tayyab
 """.format(html_content)
 
 # Create the MIMEText object
@@ -40,5 +37,5 @@ msg.attach(MIMEText(additional_text, "plain"))
 # Connect to the SMTP server and send the email
 with smtplib.SMTP("smtp.gmail.com", 587) as server:
     server.starttls()
-    server.login(sender_email, "slmoutqfqdwmbzui")  # Use an app-specific password or OAuth token
+    server.login(sender_email, "irzkrnrzmnkdywnb")  # Use an app-specific password or OAuth token
     server.sendmail(sender_email, receiver_email, msg.as_string())
