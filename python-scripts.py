ec0134ffa06fb621dface24be0aa87cadbb6add8
iSaadiqbal <139994436+iSaadiqbal@users.noreply.github.com>
2023-08-22 05:43:25 +0500

    Email

--- python-scripts.py
+++ python-scripts.py
@@ -1,55 +1,45 @@
-import os
 import smtplib
 from email.mime.text import MIMEText
 
-def send_email(subject, body):
+def send_email(subject, body, recipient_emails):
     sender_email = "saadiqbalbutt89@gmail.com"
     sender_password = "slmoutqfqdwmbzui"
-    recipient_email = "saad89.linux@gmail.com"
 
     msg = MIMEText(body)
     msg["Subject"] = subject
     msg["From"] = sender_email
-    msg["To"] = recipient_email
+    msg["To"] = ", ".join(recipient_emails)  # Concatenate email addresses
 
     try:
         with smtplib.SMTP("smtp.gmail.com", 587) as server:
             server.starttls()
             server.login(sender_email, sender_password)
-            server.sendmail(sender_email, recipient_email, msg.as_string())
+            server.sendmail(sender_email, recipient_emails, msg.as_string())
             print("Email sent successfully")
     except Exception as e:
         print("An error occurred:", e)
 
-# Replace placeholders with your actual values
-CF_API_TOKEN = "HsoabgfSbNQVeHpg30hI14GOo8mZLixzk_7HhJY8"
-RECORD_NAME = "saad.karazo.com"
-RECORD_TYPE = "CNAME"
-RECORD_CONTENT = "192.168.18.250"
-TTL = 3600
-DNS_COMMENT = "Domain verification record"
-
-# Get the trigger count from the GitHub environment variable
-trigger_count = os.getenv("GITHUB_RUN_NUMBER")
-
-# Construct the DNS record details
-dns_details = f"""
-DNS record details:
-Name: {RECORD_NAME}
-Type: {RECORD_TYPE}
-IP Address: {RECORD_CONTENT}
-TTL: {TTL}
-DNS Comment: {DNS_COMMENT}
+# Get the result of the DNS record update from the previous step
+dns_result = """
+DNS record updated by workflow. Trigger count: 5
 """
 
-# Get the result of the DNS record update from the previous step
-dns_result = f"DNS record updated by workflow. Trigger count: {trigger_count}"
+# List of recipient email addresses
+recipient_emails = ["saad89.linux@gmail.com", "usamashahid3565@gmail.com", "tayyubtahir87@gmail.com"]
 
-# Combine the details and result
-email_body = f"{dns_details}\n\n{dns_result}"
+# Extract DNS details from your workflow
+zone_id = "38b42bfdb42dbe301b6b1a27b86ac939"
+record_name = "saad.karazo.com"
+record_type = "CNAME"
+record_content = "192.168.18.250"
+ttl = 3600
 
-# Construct the email subject with trigger count
-email_subject = f"DNS Record Update Result - Trigger {trigger_count}"
+# Construct the email subject and body
+trigger_count = 5  # Replace with the actual trigger count from the workflow
+email_subject = f"DNS Record Update Result - Trigger count: {trigger_count}"
+email_body = f"{dns_result}\n\n"
+email_body += f"Zone ID: {zone_id}\nRecord Name: {record_name}\nRecord Type: {record_type}\n"
+email_body += f"Record Content: {record_content}\nTTL: {ttl}"
 
-# Call the send_email function with the subject and body
-send_email(email_subject, email_body)
+# Call the send_email function
+send_email(email_subject, email_body, recipient_emails)
