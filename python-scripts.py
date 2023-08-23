ad5a1ed39ae534ee1002019de6c68606458b073f
iSaadiqbal <139994436+iSaadiqbal@users.noreply.github.com>
2023-08-23 19:52:18 +0500

    Update python-scripts.py

--- python-scripts.py
+++ python-scripts.py
@@ -1,16 +1,13 @@
-import os
 import smtplib
 from email.mime.text import MIMEText
-
+import os
 def send_email(subject, body, recipient_emails):
     sender_email = "saadiqbalbutt89@gmail.com"
     sender_password = "slmoutqfqdwmbzui"
-
     msg = MIMEText(body)
     msg["Subject"] = subject
     msg["From"] = sender_email
     msg["To"] = ", ".join(recipient_emails)  # Concatenate email addresses
-
     try:
         with smtplib.SMTP("smtp.gmail.com", 587) as server:
             server.starttls()
@@ -19,12 +16,22 @@ def send_email(subject, body, recipient_emails):
             print("Email sent successfully")
     except Exception as e:
         print("An error occurred:", e)
-
 trigger_count = int(os.environ.get("GITHUB_RUN_NUMBER", 0))
 dns_name = os.environ.get("DNS_NAME", "")
 dns_type = os.environ.get("DNS_TYPE", "")
 dns_ip = os.environ.get("DNS_IP", "")
 
 # Get the result of the DNS record update from the previous step
-dns_result = f'''
+dns_result = f"""
 DNS record updated by workflow. Trigger count: {trigger_count}
+DNS Details:
+  Name: {dns_name}
+  Type: {dns_type}
+  IP Address: {dns_ip}
+"""
+
+# List of recipient email addresses
+recipient_emails = ["saad89.linux@gmail.com", "usamashahid3565@gmail.com", "tayyubtahir87@gmail.com"]
+
+# Call the send_email function
+send_email("DNS Record Update Result", dns_result, recipient_emails)
